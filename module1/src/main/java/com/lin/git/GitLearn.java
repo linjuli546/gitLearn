35ee8bc6c26d5740ac700e05b9d2d2fe21cba1cb
linjuli <1834902891@qq.com>
2021-06-03 12:08:41 +0800

    third commit

--- module1/src/main/java/com/lin/git/GitLearn.java
+++ module1/src/main/java/com/lin/git/GitLearn.java
@@ -11,5 +11,6 @@ public class GitLearn {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
