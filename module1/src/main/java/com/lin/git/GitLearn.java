47914c311a7230f7734848808897e2747aef7402
linjuli <1834902891@qq.com>
2021-06-03 12:04:22 +0800

    first commit

---
+++ module1/src/main/java/com/lin/git/GitLearn.java
@@ -0,0 +1,14 @@
+package com.lin.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ * user 18349
+ * date 2021/6/3
+ * time 12:00
+ * description
+ */
+public class GitLearn {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
