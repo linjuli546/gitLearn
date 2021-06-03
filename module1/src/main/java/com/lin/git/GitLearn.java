e6bd976ab815b83651f1965e750b251ca9f9c79d
linjuli <1834902891@qq.com>
2021-06-03 12:06:59 +0800

    second commit

--- module1/src/main/java/com/lin/git/GitLearn.java
+++ module1/src/main/java/com/lin/git/GitLearn.java
@@ -10,5 +10,6 @@ package com.lin.git;
 public class GitLearn {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
